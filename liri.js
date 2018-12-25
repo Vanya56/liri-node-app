ed55cc7759d5bbddfea20dd9d4db05f369b5e0c6
Vanya Levchenko <vanyalevchenko@Vanyas-MacBook-Pro.local>
2018-12-25 13:26:49 -0700

    wrote the user command and concertThis function that deals with the bands in town api

--- liri.js
+++ liri.js
@@ -35,5 +35,24 @@ function userCommand(userInput, userQuery) {
 userCommand(userInput, userQuery);
 
 function concertThis() {
-    console.log
+console.log(`/n - - - - - /n/nSEARCING FOR...${userQuery}'s next show...`);
+request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp" + bandsintown +
+response, body) {
+    if(!error && response.statusCode === 200) {
+        let userBand = JSON.parse(body);
+        if(userBand.length > 0){
+            for(i = 0; i < 1; i++){
+                // log data
+                console.log(`/nArtist: ${userBand[i].lineup[0]} /nVenue: ${userBand[i]
+                    .venue.name}/nVenue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}/nVenue
+                    City: ${userBand[i].venue.city}`)
+                    // Format moment.js MM/DD/YYYY
+                    let concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY");
+                    console.log(`Date and Time: ${concertDate}/n/n- - - - -`);
+            }
+        }else {
+            console.log("Band or Concert not found!");
+        }
+    }
+}
 }
