5929c601b30c0069f8e75d47b905f36623f95c00
Vanya Levchenko <vanyalevchenko@Vanyas-MacBook-Pro.local>
2019-01-16 08:44:22 -0700

    cleaned up code

--- liri.js
+++ liri.js
@@ -12,39 +12,42 @@ var inputs = input[3];
 
 switch (action) {
 	case "concert-this":
-	concert(inputs);
-	break;
+		concert(inputs);
+		break;
 
 	case "spotify-this-song":
-	spotify(inputs);
-	break;
+		spotify(inputs);
+		break;
 
 	case "movie-this":
-	movie(inputs);
-	break;
+		movie(inputs);
+		break;
 
 	case "do-what-it-says":
-	doit();
-	break;
+		doit();
+		break;
 };
 
 function spotify(inputs) {
 
 	var spotify = new Spotify(keys.spotifyKeys);
-		if (!inputs){
-        	inputs = 'The Sign';
-    	}
-		spotify.search({ type: 'track', query: inputs }, function(err, data) {
-			if (err){
-	            console.log('Error occurred: ' + err);
-	            return;
-	        }
+	if (!inputs) {
+		inputs = 'The Sign';
+	}
+	spotify.search({
+		type: 'track',
+		query: inputs
+	}, function (err, data) {
+		if (err) {
+			console.log('Error occurred: ' + err);
+			return;
+		}
 
-	        var songInfo = data.tracks.items;
-	        console.log("Artist(s): " + songInfo[0].artists[0].name);
-	        console.log("Song Name: " + songInfo[0].name);
-	        console.log("Preview Link: " + songInfo[0].preview_url);
-	        console.log("Album: " + songInfo[0].album.name);
+		var songInfo = data.tracks.items;
+		console.log("Artist(s): " + songInfo[0].artists[0].name);
+		console.log("Song Name: " + songInfo[0].name);
+		console.log("Preview Link: " + songInfo[0].preview_url);
+		console.log("Album: " + songInfo[0].album.name);
 	});
 }
 
@@ -52,31 +55,31 @@ function movie(inputs) {
 
 	var queryUrl = "http://www.omdbapi.com/?t=" + inputs + "&y=&plot=short&apikey=trilogy";
 
-	request(queryUrl, function(error, response, body) {
-		if (!inputs){
-        	inputs = 'Mr Nobody';
-    	}
+	request(queryUrl, function (error, response, body) {
+		if (!inputs) {
+			inputs = 'Mr Nobody';
+		}
 		if (!error && response.statusCode === 200) {
 
-		    console.log("Title: " + JSON.parse(body).Title);
-		    console.log("Release Year: " + JSON.parse(body).Year);
-		    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-		    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-		    console.log("Country: " + JSON.parse(body).Country);
-		    console.log("Language: " + JSON.parse(body).Language);
-		    console.log("Plot: " + JSON.parse(body).Plot);
-		    console.log("Actors: " + JSON.parse(body).Actors);
+			console.log("Title: " + JSON.parse(body).Title);
+			console.log("Release Year: " + JSON.parse(body).Year);
+			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+			console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+			console.log("Country: " + JSON.parse(body).Country);
+			console.log("Language: " + JSON.parse(body).Language);
+			console.log("Plot: " + JSON.parse(body).Plot);
+			console.log("Actors: " + JSON.parse(body).Actors);
 		}
 	});
 };
 
 
 function doit() {
-	fs.readFile('random.txt', "utf8", function(error, data){
+	fs.readFile('random.txt', "utf8", function (error, data) {
 
 		if (error) {
-    		return console.log(error);
-  		}
+			return console.log(error);
+		}
 
 		// Then split it by commas (to make it more readable)
 		var dataArr = data.split(",");
@@ -88,11 +91,11 @@ function doit() {
 		} else if (dataArr[0] === "concert-this") {
 			var bandname = dataArr[1].slice(1, -1);
 			concert(bandname);
-		} else if(dataArr[0] === "movie-this") {
+		} else if (dataArr[0] === "movie-this") {
 			var movie_name = dataArr[1].slice(1, -1);
 			movie(movie_name);
-		} 
-		
-  	});
+		}
+
+	});
 
 };
